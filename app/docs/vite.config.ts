0a9909a7378e095c83c5c643547e90120b095cb8
Lisovskii Ivan <dnevanek@gmail.com>
2025-12-27 16:01:14 +0300

    fix: update docs config for GitHub Pages (base path, 404.html)

--- app/docs/vite.config.ts
+++ app/docs/vite.config.ts
@@ -1,9 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 import tailwindcss from '@tailwindcss/vite';
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
@@ -11,9 +12,18 @@ export default defineConfig({
     vueDevTools(),
     tailwindcss(),
   ],
+  base: '/alien-utils/',  // GitHub Pages subpath
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        404: resolve(__dirname, '404.html'),
+      },
     },
   },
 })
